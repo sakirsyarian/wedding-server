9c31281d91baa13a41348695a54aeb5a2a0bb139
Ahmad Sarian <sakirsyarian@gmail.com>
2023-09-07 19:33:03 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+"use strict";
+
+if (process.env.NODE_ENV !== "production") {
+    require("dotenv").config();
+}
+
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+const routes = require("./routes");
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(routes);
+
+module.exports = app;
